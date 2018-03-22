fd8acb00ffba80c065b0127d1c20f9d6410aa6ad
Pukitis Valters <pvalters.mmt-b2015@fh-salzburg.ac.at>
2018-03-22 22:23:12 +0100

    add homescreen button

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,6 +27,39 @@
     daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   };
 
+  var deferredPrompt;
+
+  window.addEventListener('beforeinstallprompt', function(e) {
+      console.log('beforeinstallprompt Event fired');
+      e.preventDefault();
+
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+
+      return false;
+  });
+
+  document.getElementById('homescreen-button').addEventListener('click', function() {
+      if (deferredPrompt !== undefined) {
+          // The user has had a positive interaction with our app and Chrome
+          // has tried to prompt previously, so let's show the prompt.
+          deferredPrompt.prompt();
+
+          // Follow what the user has done with the prompt.
+          deferredPrompt.userChoice.then(function(choiceResult) {
+              console.log(choiceResult.outcome);
+
+              if (choiceResult.outcome == 'dismissed') {
+                  console.log('User cancelled home screen install');
+              } else {
+                  console.log('User added to home screen');
+              }
+
+              // We no longer need the prompt.  Clear it up.
+              deferredPrompt = null;
+          });
+      }
+  });
 
   /*****************************************************************************
    *
